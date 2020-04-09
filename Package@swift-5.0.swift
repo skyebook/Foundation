26c96becd0a06784d5a1c5c4cc34c99259d7adfc
Skye Book <skye.book@gmail.com>
2020-04-09 18:06:16 -0400

    iOS 10 min dependency to match v7 PromiseKit

--- Package@swift-5.0.swift
+++ Package@swift-5.0.swift
@@ -32,5 +32,5 @@ target.exclude += [
 pkg.targets = [target]
 
 pkg.platforms = [
-   .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
+   .macOS(.v10_10), .iOS(.v10), .tvOS(.v9), .watchOS(.v2)
 ]
